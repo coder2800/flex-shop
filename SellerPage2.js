ed5c67791e44b95dc26419c3a7359c81bd1b65d2
iampkumar <iampkumar02@gmail.com>
2021-07-08 18:13:27 +0530

    Update SellerPage2.html .css and created a new SellerPage2.js

---
+++ SellerPage2.js
@@ -0,0 +1,31 @@
+function Q11() {
+    document.getElementById('Q11').innerHTML = "&#x2780; Have physical damaged";
+    document.getElementById('Q12').innerHTML = "";
+}
+function Q12() {
+    document.getElementById('Q12').innerHTML = "&#x2780; No physical damaged";
+    document.getElementById('Q11').innerHTML = "";
+}
+function Q21() {
+    document.getElementById('Q21').innerHTML = "&#x2781; Have a valid bill";
+    document.getElementById('Q22').innerHTML = ""
+}
+function Q22() {
+    document.getElementById('Q22').innerHTML = "&#x2781; Not have a valid bill";
+    document.getElementById('Q21').innerHTML = ""
+}
+function Q31() {
+    document.getElementById('Q31').innerHTML = "&#x2782; Overall condition is good";
+    document.getElementById('Q32').innerHTML = "";
+    document.getElementById('Q33').innerHTML = "";
+}
+function Q32() {
+    document.getElementById('Q32').innerHTML = "&#x2782; Overall condition is average";
+    document.getElementById('Q31').innerHTML = "";
+    document.getElementById('Q33').innerHTML = "";
+}
+function Q33() {
+    document.getElementById('Q33').innerHTML = "&#x2782; Overall condition is below average";
+    document.getElementById('Q31').innerHTML = "";
+    document.getElementById('Q32').innerHTML = "";
+}
